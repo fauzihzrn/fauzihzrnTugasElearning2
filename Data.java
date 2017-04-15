e50d3b161455ac2db0c76f49d685244252b5fdbf
fauzihzrn <fauzihzrn96@gmail.com>
2017-04-15 10:38:35 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,15 @@
+public class Data {
+	public void contoh (int x) {
+	System.out.println(x);
+	}
+	
+	public void contoh(double x) {
+		System.out.println(x);
+		}
+		
+	public void contoh(String x) {
+		System.out.println(x);
+		}
+		
+	
+}
---
+++ polymorphisme.java
@@ -0,0 +1,8 @@
+public class polymorphisme extends Data {
+		public static void main(String[] args) {
+			Data data = new Data() ;
+			data.contoh("Fauzi Hazairin");
+			data.contoh("666");
+			data.contoh("14.058");
+			}
+}
